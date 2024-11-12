193fdd2d076c11c6e2aeaa36c86216724b77150f
Harshad <harshadbondarde2206@gmail.com>
2024-11-12 10:05:19 +0530

    added delete message route function

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const router=express.Router()
 const isAuthenticated=require('../middleware/isAuthenticated')
-const { sendMessage , getMessage }=require("../controllers/messageController")
+const { sendMessage , getMessage ,deleteMessage}=require("../controllers/messageController")
 
 router.post("/send/:id",isAuthenticated,sendMessage)
 router.get("/:id",isAuthenticated,getMessage)
+router.post("/delete/:id",isAuthenticated , deleteMessage)
 module.exports=router
