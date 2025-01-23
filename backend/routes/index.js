c248562dfb18bd53c6f580c18da5c626bcb3ade0
Harshad <harshadbondarde2206@gmail.com>
2025-01-23 14:05:26 +0530

    implemented routes for group messaging

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,10 @@ const express=require('express')
 const routes=express.Router()
 const user=require('./user')
 const message=require('./message')
+const group=require('./group') 
 
 routes.use("/user",user)
 routes.use("/message",message)
+routes.use("/group",group)
 
 module.exports=routes 
